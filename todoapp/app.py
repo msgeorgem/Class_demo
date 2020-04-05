e4a2cce65fb9af893e1751244e0776f4e8c422e5
msgeorgem <marcin.szulc01@gmail.com>
2020-04-05 17:47:18 +0200

    working version of todo app

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,18 +1,29 @@
 import sys
 
 from flask import Flask, render_template, request, jsonify, abort  # install with pip
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import expression
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://geo@localhost:5432/first_data_base'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
+
+## run flask db migrate in terminal after every change in ToDOo and then
+## all changes re inmigrations/versions folder
+## run flask db downgrade/upgrade
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    ##completed = db.Column(db.Boolean(), default=False, nullable=False)
+    completed = db.Column(db.Boolean, server_default=expression.false(), nullable=False)  ## updated existing data
+
 
     # price = db.Column(db.Float, db.CheckConstraint('price>0'))
     # name = db.Column(db.String(), nullable=False, unique=True)
@@ -21,7 +32,7 @@ class Todo(db.Model):
         return f'<Todo {self.id} {self.description}>'
 
 
-db.create_all()
+# db.create_all() ## wiil not be used if use migrations
 
 @app.route('/todos/create', methods=['POST'])  # methods not method!!!
 def create_todo():
